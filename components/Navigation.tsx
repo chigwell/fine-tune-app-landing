dc3306e1c00186139b53455663ef24525aa9e313
Eugene Evstafev <ee345@cam.ac.uk>
2025-12-11 19:22:31 +0000

    Revert "feat: remove Dashboard link from Navigation component"
    This reverts commit 61d19ee0de739e8df5a850075f89f0249bf21fe9.

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -45,6 +45,10 @@ async function getGitHubStars(): Promise<number> {
 
 export default function Navigation() {
   const navItems = [
+    {
+      name: "Dashboard",
+      link: "https://dash.fine-tune.app",
+    },
   ];
 
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
