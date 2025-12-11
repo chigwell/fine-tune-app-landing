39cafbc6ee1f3c2818361e822886d5531f702061
Eugene Evstafev <ee345@cam.ac.uk>
2025-12-11 19:16:34 +0000

    feat: remove example links from Footer and Navigation components

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -46,10 +46,6 @@ async function getGitHubStars(): Promise<number> {
 export default function Navigation() {
   const navItems = [
     {
-      name: "Example",
-      link: "#example",
-    },
-    {
       name: "Dashboard",
       link: "https://dash.fine-tune.app",
     },
